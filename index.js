6462fca379986b881b86cf3333e38f9aa3b443a7
starcradle101 <dreamerdev12@gmail.com>
2025-05-11 01:49:07 +0900

    refactor: 카페 시스템을 자판기 형태로 전체 구조 개편

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-import Customer from './src/Customer.js';
-import Barista from './src/Barista.js';
-import Cashier from './src/Cashier.js';
+import CoffeeVendingMachineApp from './src/CoffeeVendingMachineApp.js';
 
-const barista = new Barista('가배');
-const cashier = new Cashier('후니훈', barista);
-const customer = new Customer('레니');
-
-console.log(barista.introduce());
-console.log(cashier.introduce());
-console.log(customer.introduce());
-
-customer.order('에스프레소', cashier);
+const app = new CoffeeVendingMachineApp();
+app.start();
